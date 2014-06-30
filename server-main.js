615d611b3dbdd9b67d160812bad808fc9b0d75b2
Cameron Edwards <cameronprattedwards@gmail.com>
2014-06-30 09:39:12 -0400

    Create interface between sockets and REST

--- server-main.js
+++ server-main.js
@@ -1,24 +1,24 @@
-var express = require("express"),
-	bodyParser = require("body-parser"),
-	router = express.Router(),
-	app = express();
+var express = require('express'),
+bodyParser = require("body-parser"),
+router = express.Router(),
+app = express(),
+http = require('http').Server(app),
+io = require('socket.io')(http);
 
 var todos = [
-	{
-		id: 0,
-		message: "take out garbage",
-		due: new Date(2014, 5, 30),
-		isComplete: false
-	},
-	{
-		id: 1,
-		message: "call your mother",
-		due: new Date(2014, 5, 29),
-		isComplete: false
-	}
+{
+	id: 0,
+	message: "take out garbage",
+	due: new Date(2014, 5, 30),
+	isComplete: false
+},
+{
+	id: 1,
+	message: "call your mother",
+	due: new Date(2014, 5, 29),
+	isComplete: false
+}
 ];
-
-console.log("router:", router);
 
 router.param("todo", function (req, res, next, id) {
 	console.log("got todo", id);
@@ -53,6 +53,7 @@ router.post("/todos", function (req, res) {
 		req.body.isComplete = false;
 	todos.push(req.body);
 	res.send(req.body);
+	io.emit("todo added", req.body);
 });
 
 router.put("/todos/:todo", function (req, res) {
@@ -67,16 +68,86 @@ router.put("/todos/:todo", function (req, res) {
 
 	console.log("put", req.body);
 	res.send();
+	io.emit("todo updated", existing);
 });
 
 router.delete("/todos/:todo", function (req, res) {
 	console.log("delete", req.todo);
 	todos.splice(todos.indexOf(req.todo), 1);
 	res.send(req.todo);
+	io.emit("todo removed", req.todo.id);
 });
 
 app.use(bodyParser.json());
 app.use("/api", router);
 app.use(express.static(__dirname));
 
-app.listen(8081);
+http.listen(8081, function () {
+	console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	todos.forEach(function (todo) {
+		console.log('emitting todo', todo);
+		socket.emit('todo added', todo);
+	});
+
+	socket.on('todo added', function (obj) {
+		console.log('todo added', obj);
+		var todo = {};
+		for (var x in obj) {
+			if (x == "due")
+				todo[x] = new Date(obj[x]);
+			else
+				todo[x] = obj[x];
+		}
+		todo.id = todos[todos.length - 1].id + 1;
+		todos.push(todo);
+
+		io.emit('todo added', todo);
+	});
+
+	socket.on('todo updated', function (obj) {
+		console.log('todo update call', obj);
+
+		var todo = todos.filter(function (todo) {
+			return todo.id == obj.id;
+		})[0];
+
+		for (var x in obj) {
+			if (x == "due")
+				obj[x] = todo[x] = new Date(obj[x]);
+			else
+				todo[x] = obj[x];
+		}
+
+		socket.broadcast.emit("todo updated", obj);
+	});
+
+	socket.on('todo removed', function (id) {
+		console.log('todo removed', id);
+
+		var todo = todos.filter(function (todo) {
+			return todo.id == id;
+		})[0];
+
+		todos.splice(todos.indexOf(todo), 1);
+
+		socket.broadcast.emit("todo removed", id);
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
